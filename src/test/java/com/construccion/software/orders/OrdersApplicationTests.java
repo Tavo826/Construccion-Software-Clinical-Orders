2da9b86bfeaf2888b4817029f5057769d61af5ea
Gustavo Orozco <gustavo.orozcom68@gmail.com>
2025-10-16 15:12:22 -0500

    Project init

---
+++ src/test/java/com/construccion/software/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construccion.software.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
